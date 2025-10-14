8f8994c51f28549d324fcfe6ce8983d92edba8a2
Gorp900 <gorp900@googlemail.com>
2025-10-14 17:20:48 +0100

    More Lamepaue csid's confirmed

--- scripts/zones/Bastok_Markets/npcs/Lamepaue.lua
+++ scripts/zones/Bastok_Markets/npcs/Lamepaue.lua
@@ -25,49 +25,60 @@ local csInfo =
     },
     { -- BASTOK QUESTS
         { csId = 243,   type = "quest",     log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.THE_RETURN_OF_THE_ADVENTURER   },
-        { csId = 322,   type = "quest",     log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.THE_FIRST_MEETING              }, -- TODO: Confirm CSID for this, it's an optional CS
+        { csId = 322,   type = "quest",     log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.THE_FIRST_MEETING              },
         { csId = 329,   type = "quest",     log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.WISH_UPON_A_STAR               },
         { csId = 332,   type = "quest",     log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.WISH_UPON_A_STAR               },
         { csId = 334,   type = "quest",     log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.WISH_UPON_A_STAR               },
-        { csId = 0,     type = "",          log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.ALL_BY_MYSELF                  },
-        { csId = 0,     type = "",          log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.ACHIEVING_TRUE_POWER           },
+        { csId = 372,   type = "",          log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.ALL_BY_MYSELF                  },
+        -- ^ TODO: csid 372 is wrong, Can't seem to pin down the exact id used for the cutscene
+        { csId = 441,   type = "",          log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.ACHIEVING_TRUE_POWER           },
+        -- ^ TODO: 441 Seems like the logical choice for this CS, but doesn't seem to load right
         { csId = 473,   type = "quest",     log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.TOO_MANY_CHEFS                 },
-        { csId = 0,     type = "",          log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.A_PROPER_BURIAL                },
-        { csId = 0,     type = "",          log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.A_PROPER_BURIAL                },
-        { csId = 0,     type = "",          log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.A_PROPER_BURIAL                },
-        { csId = 0,     type = "",          log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.A_PROPER_BURIAL                },
-        { csId = 0,     type = "",          log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.A_PROPER_BURIAL                },
-        { csId = 0,     type = "",          log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.A_PROPER_BURIAL                },
+        { csId = 475,   type = "",          log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.A_PROPER_BURIAL                },
+        { csId = 477,   type = "",          log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.A_PROPER_BURIAL                },
+        { csId = 479,   type = "",          log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.A_PROPER_BURIAL                },
+        { csId = 481,   type = "",          log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.A_PROPER_BURIAL                },
+        { csId = 483,   type = "",          log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.A_PROPER_BURIAL                },
+        { csId = 485,   type = "",          log = xi.questLog.BASTOK,       requirement = xi.quest.id.bastok.A_PROPER_BURIAL                },
     },
     { -- OTHER QUESTS
         { csId = 342,   type = "quest",     log = xi.questLog.JEUNO,        requirement = xi.quest.id.jeuno.BEAT_AROUND_THE_BUSHIN          },
         { csId = 402,   type = "quest",     log = xi.questLog.OTHER_AREAS,  requirement = xi.quest.id.otherAreas.CONFESSIONS_OF_A_BELLMAKER },
-        { csId = 0,     type = "",          log = xi.questLog.OTHER_AREAS,  requirement = xi.quest.id.otherAreas.PICTURE_PERFECT            },
-        { csId = 0,     type = "",          log = xi.questLog.OTHER_AREAS,  requirement = xi.quest.id.otherAreas.PICTURE_PERFECT            },
-        { csId = 0,     type = "",          log = xi.questLog.OTHER_AREAS,  requirement = xi.quest.id.otherAreas.PICTURE_PERFECT            },
-        { csId = 0,     type = "",          log = xi.questLog.OTHER_AREAS,  requirement = xi.quest.id.otherAreas.PICTURE_PERFECT            },
+        { csId = 403,   type = "",          log = xi.questLog.OTHER_AREAS,  requirement = xi.quest.id.otherAreas.PICTURE_PERFECT            },
+        { csId = 404,   type = "",          log = xi.questLog.OTHER_AREAS,  requirement = xi.quest.id.otherAreas.PICTURE_PERFECT            },
+        { csId = 405,   type = "",          log = xi.questLog.OTHER_AREAS,  requirement = xi.quest.id.otherAreas.PICTURE_PERFECT            },
+        { csId = 406,   type = "",          log = xi.questLog.OTHER_AREAS,  requirement = xi.quest.id.otherAreas.PICTURE_PERFECT            },
+        -- ^ TODO: The above 4 cutscenes for PICTURE_PERFECT might all need some extraInfo Var. I beleive the players Adventuring Fellow is supposed to appear in the cutscenes.
         { csId = 434,   type = "quest",     log = xi.questLog.AHT_URHGAN,   requirement = xi.quest.id.ahtUrhgan.NO_STRINGS_ATTACHED         },
-        { csId = 0,     type = "",          log = xi.questLog.AHT_URHGAN,   requirement = xi.quest.id.ahtUrhgan.PUPPETMASTER_BLUES          },
-        { csId = 0,     type = "",          log = xi.questLog.AHT_URHGAN,   requirement = xi.quest.id.ahtUrhgan.PUPPETMASTER_BLUES          },
+        { csId = 437,   type = "",          log = xi.questLog.AHT_URHGAN,   requirement = xi.quest.id.ahtUrhgan.PUPPETMASTER_BLUES          },
+        { csId = 439,   type = "",          log = xi.questLog.AHT_URHGAN,   requirement = xi.quest.id.ahtUrhgan.PUPPETMASTER_BLUES          },
         { csId = 490,   type = "quest",     log = xi.questLog.JEUNO,        requirement = xi.quest.id.jeuno.COMEBACK_QUEEN                  },
-        { csId = 0,     type = "",          log = ,                         requirement =                                                   }, -- Dancer AF pieces, part of a hiddenQuest, TODO: Implement Requirement checking for hiddenQuest? or see what requirement is best.
-        { csId = 0,     type = "",          log = ,                         requirement =                                                   }, -- ^ as above. Also might need extraInfo var for writing the right piece of equipment in the CS text. see: quests/hiddenQuests/crated_dancer_artifact.lua
+        { csId = 492,   type = "",          log = TODO,                     requirement = TODO_DANCER_ATTIRE                                },
+        -- ^ Dancer AF pieces, part of a hiddenQuest, TODO: Implement Requirement checking for hiddenQuest? or see what requirement is best.
+        { csId = 497,   type = "",          log = TODO,                     requirement = TODO_DANCER_ATTIRE                                },
+        -- ^ as above. Also might need extraInfo var for writing the right piece of equipment in the CS text. see: quests/hiddenQuests/crated_dancer_artifact.lua
         { csId = 0,     type = "",          log = xi.questLog.CRYSTAL_WAR,  requirement = DRAFTED_BY_THE_DUCHY                              },
         { csId = 0,     type = "",          log = xi.questLog.CRYSTAL_WAR,  requirement = BATTLE_ON_A_NEW_FRONT                             },
-        { csId = 0,     type = "",          log = xi.questLog.JEUNO,        requirement = VW_OP_126_QUFIM_INCURSION                         }, -- Unimplemented? Requirement doesn't currently exist but should be this.
-        { csId = 24,    type = "roe",       log = ,                         requirement =                                                   }, -- TODO: check csid, also new type of requirement, this CS is seen after tutorial RoE completed, see NPC: Isakoth.lua
-        { csId = 0,     type = "",          log = ,                         requirement = TRUST_MUMOR                                       }, -- TODO: This CS is tied to the sunbreeze event 2014, how do we even track this if not check if the player has the reward?
-        { csId = 594,   type = "unity",     log = ,                         requirement = UNITY_CONCORD                                     }, -- TODO: Similar to above roe type, how best to tackle this requirement?
-        { csId = 598,   type = "unity",     log = ,                         requirement = UNITY_CONCORD                                     }, --  ^ as above, might need extraInfo var for showing correct UC_Leader convo
-        { csId = 0,     type = "",          log = ,                         requirement = TRUST_MUMOR_II                                    }, -- TODO: This CS is tied to the sunbreeze event 2015, how do we even track this if not check if the player has the reward?
+        { csId = 0,     type = "",          log = xi.questLog.JEUNO,        requirement = VW_OP_126_QUFIM_INCURSION                         },
+        -- ^ TODO: Above 3: Unimplemented? Requirement doesn't currently exist but should be this.
+        { csId = 24,    type = "roe",       log = TODO,                     requirement = TODO_RECORDS_OF_EMINENCE_QUEST                    },
+        -- ^ TODO: check csid, also new type of requirement, this CS is seen after tutorial RoE completed, see NPC: Isakoth.lua
+        { csId = 0,     type = "",          log = TODO,                     requirement = TODO_TRUST_MUMOR                                  },
+        -- ^ TODO: This CS is tied to the sunbreeze event 2014, how do we even track this if not check if the player has the reward?
+        { csId = 595,   type = "unity",     log = TODO,                     requirement = TODO_UNITY_CONCORD                                },
+        -- ^ TODO: Similar to above roe type, how best to tackle this requirement? This might also need extraInfo var, as the model for the voodoo doll is wrong
+        { csId = 0,     type = "",          log = TODO,                     requirement = TODO_UNITY_CONCORD                                },
+        -- ^ TODO: Apparently there is a 2nd Unity Cutscene here, no idea what it is
+        { csId = 0,     type = "",          log = TODO,                     requirement = TODO_TRUST_MUMOR_II                               },
+        -- ^ TODO: This CS is tied to the sunbreeze event 2015, how do we even track this if not check if the player has the reward?
     },
     { -- ADD-ON SCENARIOS
         { csId = 30025, type = "mission",   log = xi.mission.log_id.AMK,    requirement = xi.mission.id.amk.DRENCHED_IT_BEGAN_WITH_A_RAINDROP, extraInfo = "mogHouse" },
         { csId = 30026, type = "mission",   log = xi.mission.log_id.AMK,    requirement = xi.mission.id.amk.DRENCHED_IT_BEGAN_WITH_A_RAINDROP, extraInfo = "mogHouse" },
         { csId = 30035, type = "mission",   log = xi.mission.log_id.ROV,    requirement = xi.mission.id.rov.RHAPSODIES_OF_VANADIEL },   
         { csId = 30036, type = "mission",   log = xi.mission.log_id.ROV,    requirement = xi.mission.id.rov.WHAT_LIES_BEYOND },
-        { csId = 30039, type = "mission",   log = xi.mission.log_id.ROV,    requirement = xi.mission.id.rov.EVER_FORWARD },
-        { csId = 30040, type = "mission",   log = xi.mission.log_id.ROV,    requirement = xi.mission.id.rov.EVER_FORWARD },-- TODO: Check this csid
+        { csId = 30039, type = "mission",   log = xi.mission.log_id.ROV,    requirement = xi.mission.id.rov.THE_BEGINNING },
+        { csId = 30040, type = "mission",   log = xi.mission.log_id.ROV,    requirement = xi.mission.id.rov.EVER_FORWARD },
         { csId = 0,     type = "",          log = xi.mission.log_id.ROV,    requirement = xi.mission.id.rov.DARKNESS_BECKONS }, -- Not implemented
         { csId = 0,     type = "",          log = xi.mission.log_id.ROV,    requirement = xi.mission.id.rov.DARKNESS_BECKONS }, -- Not implemented
         { csId = 0,     type = "",          log = xi.mission.log_id.ROV,    requirement = xi.mission.id.rov.DARKNESS_BECKONS }, -- Not implemented
